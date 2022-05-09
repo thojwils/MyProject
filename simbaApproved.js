75c36a0c04bed547eb94dcf5190cd652b0f77691
TJ Wilson <87959418+thojwils@users.noreply.github.com>
2022-05-08 23:24:46 -0400

    Updated Simba's World
    Added animation for when you click on the rescue cat link. Added a gif toggle (wip)

--- simbaApproved.js
+++ simbaApproved.js
@@ -1,10 +1,43 @@
 "use strict";
+
 const catRescue = document.querySelector(`.simba-approved`);
 const simbaApproved = function () {
-  alert(`Simba Approves`);
-  window.open(
-    "https://www.google.com/search?q=cat+rescue+shelters+near+me",
-    "_blank"
-  );
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  Toast.fire({
+    imageUrl: "/simbaPics/simba_stretch_thumbnail.jpg",
+    icon: "success",
+    title: "Simba meows in approval. <br> New tab opening in Google!",
+  });
+};
+const showPosition = function () {
+  setTimeout(() => {
+    window.open(
+      "https://www.google.com/search?q=cat+rescue+shelters+near+me",
+      "_blank"
+    );
+  }, 2000);
 };
 catRescue.addEventListener(`click`, simbaApproved);
+
+// Gif toggle
+const catGif = document.querySelector(`.catGif`);
+const toggleSwitch = document.querySelector(`.switch`);
+function toggle() {
+  if (document.getElementById("MyId").className == "hide")
+    document.getElementById("MyId").className = "show";
+  else document.getElementById("MyId").className = "hide";
+}

f7fddba94f3b4663c1866e5721252f0829453856
TJ Wilson <87959418+thojwils@users.noreply.github.com>
2022-05-17 18:46:53 -0400

    Completed NewsAPI connection
    Keeps fetch request current by using new Date(). Appends query in the input form to fetch request. Loops over res object and renders img, title, author, and an hours since calculation.

--- scripts/fetchNews.js
+++ scripts/fetchNews.js
@@ -1,57 +1,40 @@
-//techcrunch.vercel.app/articles?tag={tag-name}
-
-// Code for fetching stocks
-
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Host": "yh-finance.p.rapidapi.com",
-//     "X-RapidAPI-Key": "882f8ea9a8msh2a1982cd5a5a8b3p124b8ajsncc453bc41177",
-//   },
-// };
-
-// fetch(
-//   "https://yh-finance.p.rapidapi.com/auto-complete?q=tesla&region=US",
-//   options
-// )
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
-
 //News API
-
-// Generate new Date to plug into the fetch so that it is always pulling current news
+// Global Variables
+const searchInput = document.querySelector(`.news-input`);
 const dateToday = new Date();
-let searchTerm = "Tech";
-//Need to create an input field to plug into the q="term&" query
-const todaysNews =
-  "https://newsapi.org/v2/everything?" +
-  //Need to figure out how to input from form
-  `q=${searchTerm}&` +
-  `from=${dateToday}&` +
-  "sortBy=popularity&" +
-  "apiKey=7b1e6e2a55ea4718b0451d26964d3e38";
-
-// Attach to button attached to an input form.
+let searchTerm = ""; // How should I capture this parameter from the input of the searchInput field? I need this to read from the event listener (enter submit).
 let articles = "";
-// Request to the api.
-// async function getResponse() {
-//   const response = await fetch(todaysNews);
-//   const data = await response.json();
-//   const articles = data.articles;
-//   console.log(response);
-// }
-// getResponse();
+// API URL Request
 const fetchNews = async () => {
   try {
-    const res = await fetch(todaysNews);
+    const res = await fetch(
+      "https://newsapi.org/v2/everything?" +
+        `q=${searchTerm}&` +
+        `from=${dateToday}&` +
+        "sortBy=popularity&" +
+        "apiKey=7b1e6e2a55ea4718b0451d26964d3e38"
+    );
     console.log(res.ok);
     const data = await res.json();
     const articles = data.articles;
     console.log(articles);
-    for (let i = 0; i < 6; i++) {
-      console.log([articles[i].title, articles[i].url]);
-      const news = [articles[i].title, articles[i].url];
+    //Time for calc hours since post
+
+    for (let i = 0; i < 20; i++) {
+      // console.log([articles[i].title, articles[i].url]);
+      // const news = [articles[i].title, articles[i].url];
+      const today = new Date();
+      const todayISO = today.toISOString();
+      const start = new Date(dateToday).getTime();
+      const endDate = data.articles[i].publishedAt;
+      const end = new Date(endDate).getTime();
+      const milliseconds = Math.abs(end - start).toString();
+      const seconds = parseInt(milliseconds / 1000);
+      const minutes = parseInt(seconds / 60);
+      const hours = parseInt(minutes / 60);
+      const time = (hours % 24) + " hour's ago";
+      console.log(time);
+      // let timeDif = today - articles[i].publishedAt;
       const newsFeed = document.querySelector(`#newsFeedStart`);
       newsFeed.insertAdjacentHTML(
         `afterbegin`,
@@ -65,25 +48,23 @@ const fetchNews = async () => {
         `<div onclick="location.href = '${articles[i].url}'" class="news_container" id="${i}">
   <div class="card">
     <div class="card__header">
-      <img src="https://source.unsplash.com/600x400/?computer" alt="card__image" class="card__image" width="600">
+      <img src="${articles[i].urlToImage}" alt="" class="card__image" width="600">
     </div>
     <div class="card__body">
-      <span class="tag tag-blue">Search Term</span>
-      <h4>${articles[i].title}</h4>
-      <p>Description?</p>
+      <span class="tag tag-blue">${searchTerm}</span>
+      <h6>${articles[i].title}</h6>
     </div>
     <div class="card__footer">
       <div class="user">
         <div class="author__info">
-          <h4>${articles[i].author}</h4>
-          <small>${articles[i].publishedAt}</small>
+          <h5>Written by: ${articles[i].author}</h5>
+          <small>${time}</small>
         </div>
       </div>
     </div>
   </div>
 </div>
 <div>
-<hr class="topborderAngle"></hr>
 </div>`
       );
     }
@@ -98,10 +79,13 @@ const fetchNews = async () => {
   }
 };
 
-const newsInput = document.querySelector(`.news-input`);
+searchInput.addEventListener(`keypress`, function (e) {
+  if (e.key === "Enter") {
+    searchTerm = searchInput.value;
+    fetchNews();
+  }
+});
 // Run this function on input confirmation (enter button or button click (need to add a new button first))
-fetchNews();
-
 // Calls on input in search bar
 // newsInput.fetchNews();
 

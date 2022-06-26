aee7c9d87accdb79ed74bb6af71dcef494a421c8
TJ Wilson <87959418+thojwils@users.noreply.github.com>
2022-06-25 20:03:54 -0400

    Update mainBody.js

--- scripts/mainBody.js
+++ scripts/mainBody.js
@@ -1,10 +1,10 @@
 // H3 Collapse Toggle
 //      h1[0] should start open (added class on HTML element by default)
-document.querySelectorAll(`h3`).forEach((h3) => {
-  h3.addEventListener(`click`, () => {
-    const accordionContent = h3.nextElementSibling;
-    h3.classList.toggle(`h3--active--after`);
-    if (h3.classList.contains(`h3--active--after`)) {
+document.querySelectorAll(`h2`).forEach((h2) => {
+  h2.addEventListener(`click`, () => {
+    const accordionContent = h2.nextElementSibling;
+    h2.classList.toggle(`h2--active--after`);
+    if (h2.classList.contains(`h2--active--after`)) {
       accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
     } else {
       accordionContent.style.maxHeight = 0;

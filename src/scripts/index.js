98bcde8bffa9b2b6515e37517acc017b74b00063
TJ Wilson <87959418+thojwils@users.noreply.github.com>
2022-08-14 14:13:22 -0400

    8/14

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@ import "./countdownTimer";
 import "./fetchJoke";
 import "./fetchNews";
 import "./mainBody";
+import "./topNav";
 // Do not want to compile js for Bootstrap page (Simba's World)
 // import "./simbaApproved";
-import "./topNav";
 console.log(`Babel Compiled and Webpack Bundled`);

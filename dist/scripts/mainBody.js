c66eb31ca04dbaf05734db2736e5d9547d9b7bc9
TJ Wilson <87959418+thojwils@users.noreply.github.com>
2022-08-26 14:27:41 -0400

    Revert "updated mainBody.js"
    This reverts commit b5574ef5aab89d5980ccb7268483132220ceb697.

--- dist/scripts/mainBody.js
+++ dist/scripts/mainBody.js
@@ -5,53 +5,35 @@ const header2 = document.querySelector(`#header2`);
 const header3 = document.querySelector(`#header3`);
 const accordionContent1 = header1.nextElementSibling;
 const accordionContent2 = header2.nextElementSibling;
-const accordionContent3 = header3.nextElementSibling; // Toggle header IDs to add px scroll height to match the height of the text block
+const accordionContent3 = header3.nextElementSibling;
 
-header1.addEventListener("click", function () {
-  accordionContent1.classList.toggle("tab-open");
-
-  if (accordionContent1.classList.contains("tab-open")) {
-    accordionContent1.style.maxHeight = accordionContent1.scrollHeight + "px";
+const toggleHeight = function (el) {
+  if (!el.previousElementSibling.classList.contains("tab-open")) {
+    el.style.maxHeight = el.scrollHeight + "px";
   } else {
-    accordionContent1.style.maxHeight = "0px";
+    el.style.maxHeight = 0;
   }
+};
+
+const toggleClass = function (el) {
+  el.classList.toggle("tab-open");
+};
+
+header1.addEventListener(`click`, toggleHeight(header1));
+header2.addEventListener(`click`, toggleHeight(header2));
+header3.addEventListener(`click`, toggleHeight(header3));
+header1.addEventListener(`click`, () => {
+  toggleHeight(accordionContent1);
+  toggleClass(header1);
 });
-header2.addEventListener("click", function () {
-  accordionContent2.classList.toggle("tab-open");
-
-  if (accordionContent2.classList.contains("tab-open")) {
-    accordionContent2.style.maxHeight = accordionContent2.scrollHeight + "px";
-  } else {
-    accordionContent2.style.maxHeight = "0px";
-  }
+header2.addEventListener(`click`, () => {
+  toggleHeight(accordionContent2);
+  toggleClass(header2);
 });
-header3.addEventListener("click", function () {
-  accordionContent3.classList.toggle("tab-open");
-
-  if (accordionContent3.classList.contains("tab-open")) {
-    accordionContent3.style.maxHeight = accordionContent3.scrollHeight + "px";
-  } else {
-    accordionContent3.style.maxHeight = "0px";
-  }
-}); // const toggleClass = function (el) {
-//   el.classList.toggle("tab-open");
-// };
-// header1.addEventListener(`click`, toggleHeight(header1));
-// header2.addEventListener(`click`, toggleHeight(header2));
-// header3.addEventListener(`click`, toggleHeight(header3));
-// header1.addEventListener(`click`, () => {
-//   toggleHeight(accordionContent1);
-//   toggleClass(header1);
-// });
-// header2.addEventListener(`click`, () => {
-//   toggleHeight(accordionContent2);
-//   toggleClass(header2);
-// });
-// header3.addEventListener(`click`, () => {
-//   toggleHeight(accordionContent3);
-//   toggleClass(header3);
-// });
-//Scrolling down to projects section and adding a little bit of px height between
+header3.addEventListener(`click`, () => {
+  toggleHeight(accordionContent3);
+  toggleClass(header3);
+}); //Scrolling down to projects section and adding a little bit of px height between
 
 const naviagtionHeight = document.querySelector(`.topnav`).offsetHeight;
 document.documentElement.style.setProperty("--scroll-padding", naviagtionHeight + "px");

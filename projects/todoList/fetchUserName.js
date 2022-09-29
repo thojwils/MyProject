86b96da5b799e3d12720e1c5293f0d5d7b5f2083
TJ Wilson <87959418+thojwils@users.noreply.github.com>
2022-09-28 21:44:15 -0400

    9/28

--- projects/todoList/fetchUserName.js
+++ projects/todoList/fetchUserName.js
@@ -3,9 +3,16 @@ console.log("page loaded");
 const userNameButton = document.getElementById("userNameButton");
 const userNameTitle = document.getElementById("");
 let userName;
+// Need to check if this works
+if (userName === null) {
+  document.querySelector(".todo-title").innerHTML = `User's Todo List`;
+}
 // Change Title function
 const changeTitle = function () {
   console.log("change title");
+  if (userName === null) {
+    userName = "User";
+  }
   userName = localStorage.getItem("User Name");
   document.querySelector(".todo-title").innerHTML = `${userName}'s Todo List`;
 };
